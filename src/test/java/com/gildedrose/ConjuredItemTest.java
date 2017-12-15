62e93d100fd32809e4e1ccfd4fcf0243fbbe21c2
Lionel Durand <lionel2.durand@orange.com>
2017-12-15 11:36:31 +0100

    Ended kata with additional feature

--- src/test/java/com/gildedrose/ConjuredItemTest.java
+++ src/test/java/com/gildedrose/ConjuredItemTest.java
@@ -18,4 +18,28 @@ public class ConjuredItemTest {
 		assertThat(gildedRose.items).extracting("quality").containsExactly(4);
 	}
 
+	@Test
+	public void conjuredItemQualityNeverLessThan0() throws Exception {
+		// Given
+		Item[] items = new Item[] { new Item("Conjured Mana Cake", 3, 1) };
+		GildedRose gildedRose = new GildedRose(items);
+
+		// When
+		gildedRose.updateQuality();
+		// Then
+		assertThat(gildedRose.items).extracting("quality").containsExactly(0);
+	}
+
+	@Test
+	public void conjuredItemWithNegativeCellInDegradesQualityTwice() throws Exception {
+		// Given
+		Item[] items = new Item[] { new Item("Conjured Mana Cake", -1, 6) };
+		GildedRose gildedRose = new GildedRose(items);
+
+		// When
+		gildedRose.updateQuality();
+		// Then
+		assertThat(gildedRose.items).extracting("quality").containsExactly(2);
+	}
+
 }
